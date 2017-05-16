54b96eb9f8d887f25851d89faf92f76f52cd01bb
SarCasm <sarcasm008@gmail.com>
2017-05-16 20:40:02 +0300

    Add Travis CI badge to readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Freelance Bootcamp Homepage!';
+  title = 'Freelance Bootcamp Homepage2!';
 }
