52fc151a7c7010cb30aca27b8f1fdc539b5800bc
SarCasm <sarcasm008@gmail.com>
2017-05-16 22:29:29 +0300

    Add document objects and view them on html

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document'
 
 @Component({
   selector: 'documents',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
 })
 
 export class DocumentsComponent {
-  
+  documents: Document[] = [
+    {
+      title: 'My First Doc',
+      description: 'Lorem ipsum',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My Second Doc',
+      description: 'Lorem ipsum',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My Last Doc',
+      description: 'Lorem ipsum',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    }
+  ]
 }
