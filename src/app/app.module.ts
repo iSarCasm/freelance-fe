be3bb7e3f95b13a3cb794b70629064b2e12ffd6e
SarCasm <sarcasm008@gmail.com>
2017-05-17 13:02:02 +0300

    Proposal show component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposals/proposal-list.component';
 import { ProposalNewComponent } from './proposals/proposal-new.component';
+import { ProposalShowComponent } from './proposals/proposal-show.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -17,7 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
     HomepageComponent,
     DocumentsComponent,
     ProposalListComponent,
-    ProposalNewComponent
+    ProposalNewComponent,
+    ProposalShowComponent
   ],
   imports: [
     AppRoutingModule,
