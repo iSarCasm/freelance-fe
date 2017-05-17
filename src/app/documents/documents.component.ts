bdda61e29f043eaec51a96f19f1c0656fc70423c
SarCasm <sarcasm008@gmail.com>
2017-05-17 19:01:11 +0300

    Card style for docs

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -13,21 +13,21 @@ export class DocumentsComponent {
       description: 'Lorem ipsum',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     },
     {
       title: 'My Second Doc',
       description: 'Lorem ipsum',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     },
     {
       title: 'My Last Doc',
       description: 'Lorem ipsum',
       file_url: 'http://google.com',
       updated_at: '11/11/16',
-      image_url: 'http://google.com'
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg'
     }
   ]
 }
