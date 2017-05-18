85e5d2d6dd247361130e70cafd81cb3eb05b1cd9
SarCasm <sarcasm008@gmail.com>
2017-05-19 00:09:03 +0300

    Creating proposals

--- src/app/proposals/proposal-new.component.ts
+++ src/app/proposals/proposal-new.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   selector: 'proposal-new',
   templateUrl: 'proposal-new.component.html',
-  styleUrls: ['proposal-new.component.css']
+  styleUrls: ['proposal-new.component.css'],
+  providers: [ ProposalService ]
 })
 
 export class ProposalNewComponent {
-  proposal = new Proposal()
+  proposal: Proposal = new Proposal;
+  submitted: boolean = false;
+
+  constructor(
+    private proposalService: ProposalService
+  ) {}
+
+  createProposal(proposal: Proposal) {
+    this.proposalService.createProposal(proposal)
+        .subscribe(
+          data => { return true },
+          error => {
+            console.log("Error saving proposal");
+            return Observable.throw(error);
+          }
+        )
+    this.submitted = true;
+  }
 }
